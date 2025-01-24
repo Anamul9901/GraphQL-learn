c58e6334f4de999d0b840504e2e11aa502bd0a82
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2025-01-25 02:02:38 +0600

    Client query and auto run server

---
+++ client-Query.ts
@@ -0,0 +1,11 @@
+
+// localHost ba client theke aivabe query krte hobe.
+
+// find all product name, price and type
+// query Products {
+  //   products {
+    //     name
+    //     price
+    //     __typename // er maddhome data er type dekha jabe.
+    //   }
+    // }
